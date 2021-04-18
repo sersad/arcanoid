7cec1ae3815e206ad0653125c7ccb1d20bd2dbf1
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2021-04-18 19:34:07 +0300

    add global lives move pygame init to global move sound to global

--- arcanoid.py
+++ arcanoid.py
@@ -11,6 +11,7 @@ SIZE = WIDTH, HEIGHT = 1024, 768
 BRICK_SIZE = 40, 20
 HANDLE_SIZE = 160, 10
 BALL_RADIUS = 5
+LIVES = 5
 
 # https://github.com/pygame/pygame/blob/main/src_py/colordict.py
 WHITE = pygame.Color("WHITE")
@@ -37,7 +38,18 @@ BRICK_COLORS = {1: (WHITE, GRAY),
 BALL_COLORS = YELLOW
 
 
-def load_image(name, colorkey=None):
+pygame.init()
+sound_handle = pygame.mixer.Sound('sound/Arkanoid SFX (2).wav')
+sound_brick_dead = pygame.mixer.Sound('sound/Arkanoid SFX (1).wav')
+sound_spawn = pygame.mixer.Sound('sound/Arkanoid SFX (4).wav')
+sound_brick_no_dead = pygame.mixer.Sound('sound/Arkanoid SFX (7).wav')
+sound_game_over = pygame.mixer.Sound('sound/05_-_Arkanoid_-_ARC_-_Game_Over.ogg')
+
+# pygame.mixer.music.load('sound/02_-_Arkanoid_-_ARC_-_Game_Start.ogg')
+# pygame.mixer.music.play()
+
+
+def load_image(name: str, colorkey=None):
     fullname = os.path.join('data', name)
     #  если файл не существует, то выходим
     if not os.path.isfile(fullname):
@@ -55,20 +67,25 @@ def load_image(name, colorkey=None):
 
 
 class World:
-    def __init__(self, screen):
+    """
+    Основное игровое поле
+    """
+    def __init__(self, screen) -> None:
         self.screen = screen
         self.clock = pygame.time.Clock()
+        self.lives = LIVES
         self.bricks = []
+        self.handle = Handle((WIDTH // 2 - HANDLE_SIZE[0] // 2, HEIGHT - HANDLE_SIZE[1] - 40))
+        self.balls = []
+        self.spawn_ball()
+        # генерация карты
         for x in range(20):
             for y in range(5):
                 lives = randint(1, 4)
                 self.bricks.append(Brick(((10 + (BRICK_SIZE[0] + 10) * x, 40 + (BRICK_SIZE[1] + 10) * y)),
                                          lives=lives))
-        self.handle = Handle((WIDTH // 2 - HANDLE_SIZE[0] // 2, HEIGHT - HANDLE_SIZE[1] - 40))
-        self.balls = []
-        self.spawn_ball()
 
-    def draw(self):
+    def draw(self) -> None:
         self.handle.draw(self.screen)
         for brick in self.bricks:
             brick.draw(self.screen)
@@ -76,21 +93,24 @@ class World:
         for ball in self.balls:
             ball.draw(self.screen)
 
-    def update(self):
+    def update(self) -> None:
         time = self.clock.get_time()
         self.handle.update(time)
         for i in self.balls:
             i.update(time)
-
+        # если мяч вылетел за пределы нижней линии то удаляем его
         for ball in self.balls:
             if ball.position[1] > HEIGHT:
                 self.balls.remove(ball)
-
+        # если все мячи кончились то -1 жизнь и добавляем мяч
         if not self.balls:
             self.handle = Handle((WIDTH // 2 - 30, HEIGHT - 10 - 40))
             self.spawn_ball()
+            self.lives -= 1
+            if self.lives < 1:
+                self.game_over()
             return
-
+        # проверяем столкновения с блоков с мячом
         new_bricks = []
         for brick in self.bricks:
             for ball in self.balls:
@@ -107,7 +127,7 @@ class World:
                 new_bricks.append(brick)
 
         self.bricks = new_bricks
-
+        # проверяем столкновение мячей с ракеткой, если во время движения то меняем скорости мячей
         for ball in self.balls:
             if self.handle.is_inside(ball.position):
                 self.handle.check_collision(ball)
@@ -122,14 +142,31 @@ class World:
         self.clock.tick(60)
 
     def spawn_ball(self):
+        """
+        Добавления мяча
+        :return:
+        """
         position = self.handle.position[:]
         position[0] += 30
         position[1] -= 5
         self.balls.append(Ball(position))
         sound_spawn.play()
 
+    def game_over(self) -> None:
+        """
+        TODO: Надо написать
+        Конец игры
+        :return:
+        """
+        print('game over')
+
+        pass
+
 
 class Brick:
+    """
+    Класс блоков
+    """
     w_size = BRICK_SIZE[0]
     h_size = BRICK_SIZE[1]
 
@@ -137,22 +174,24 @@ class Brick:
         self.position = list(position)
         self.lives = lives
         self.color = BRICK_COLORS[lives]
-        # пока не нужно
-        # self.rect = pygame.Rect(*self.position, BRICK_SIZE[0], BRICK_SIZE[1])
 
-    def draw(self, screen):
+    def draw(self, screen: pygame.Surface) -> None:
+        """
+        Рисование блока с градиентной заливкой и тенью.
+        Рисуем Surface 2x2, пару линий в нем и растягиваем его до размеров блока.
+        :param screen:
+        :return:
+        """
         self.color = BRICK_COLORS[self.lives]
         pygame.gfxdraw.box(screen, pygame.Rect((self.position[0] + 1, self.position[1] + 1),
                                                (self.w_size + 1, self.h_size + 1)), DARKGRAY)
-        # pygame.gfxdraw.box(screen, pygame.Rect(self.position, (self.w_size, self.h_size)), self.color)
-
-        colour_rect = pygame.Surface((2, 2))  # tiny! 2x2 bitmap
-        pygame.draw.line(colour_rect, self.color[1], (0, 0), (0, 1))  # left colour line
+        colour_rect = pygame.Surface((2, 2))
+        pygame.draw.line(colour_rect, self.color[1], (0, 0), (0, 1))
         pygame.draw.line(colour_rect, self.color[0], (1, 0), (1, 1))
         colour_rect = pygame.transform.smoothscale(colour_rect, (self.w_size, self.h_size))
         screen.blit(colour_rect, self.position)
 
-    def draw_destroy(self, screen):
+    def draw_destroy(self, screen: pygame.Surface) -> None:
         """
         Эффект уменьшения жизней у блока или его смерти.
         Если жизнь просто уменьшилась, отрисовывается рамка старого цвета вокруг блока
@@ -197,6 +236,9 @@ class Brick:
 
 
 class Handle(Brick):
+    """
+    Класс ракетки, в целом это тоже что и обычный блок и отличается одним методом
+    """
     w_size = HANDLE_SIZE[0]
     h_size = HANDLE_SIZE[1]
 
@@ -209,6 +251,9 @@ class Handle(Brick):
 
 
 class Ball:
+    """
+    Класс мяча
+    """
     def __init__(self, position, color=BALL_COLORS):
         self.position = list(position)
         self.color = color
@@ -233,17 +278,6 @@ class Ball:
 
 
 def main():
-    global sound_handle, sound_brick_dead, sound_brick_no_dead, sound_spawn, sound_game_over
-    pygame.init()
-    # pygame.mixer.music.load('sound/02_-_Arkanoid_-_ARC_-_Game_Start.ogg')
-    # pygame.mixer.music.play()
-
-    sound_handle = pygame.mixer.Sound('sound/Arkanoid SFX (2).wav')
-    sound_brick_dead = pygame.mixer.Sound('sound/Arkanoid SFX (1).wav')
-    sound_spawn = pygame.mixer.Sound('sound/Arkanoid SFX (4).wav')
-    sound_brick_no_dead = pygame.mixer.Sound('sound/Arkanoid SFX (7).wav')
-    sound_game_over = pygame.mixer.Sound('sound/05_-_Arkanoid_-_ARC_-_Game_Over.ogg')
-
     screen = pygame.display.set_mode(SIZE)
     world = World(screen)
 
